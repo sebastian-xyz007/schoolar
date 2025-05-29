e288484e3687accd99eaa0c5dc143326feebc896
sebastian muñoz <sebastianmunoz678@gmail.com>
2025-05-29 12:52:36 -0500

    Shoolar ajustes

--- src/list_users.php
+++ src/list_users.php
@@ -1,7 +1,6 @@
 <?php
-include('../config/database.php');
-
-    ?>
+    include('../config/database.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,37 +14,41 @@ include('../config/database.php');
         <tr>
             <th>Firstname</th>
             <th>Lastname</th>
-            <th>E_mail</th>
+            <th>E-mail</th>
             <th>Status</th>
             <td>...</td>
         </tr>
-        
         <?php
-        //Here Code
-        $sql="select firstname, lastname, email, case when status = true then 'Active' else 'no active' end as status
-	    from users";
-        $res = pg_query($conn, $sql);
-        if(!$res){
-            echo "Query error";
-            exit;
-        }
-        while($row = pg_fetch_ASSOC($res) ){
-            echo "<tr>";
-            echo "<td>".$row['firstname']."</td>";
-            echo "<td>".$row['lastname']."</td>";
-            echo "<td>".$row['email']."</td>";
-            echo "<td>".$row['status']."</td>";
-            echo "<td>";
-            echo "<a href=''><img src='icons/usuario.png' width='15'></a>";
-            echo "<a href=''><img src='icons/delete.png' width='15'></a>";
-            echo "<a href=''><img src='icons/search.png' width='15'></a>";
-            echo "</td>";
-            echo "</tr>";
-        }
-        ?>
-            
+            //Here code
+            $sql = "
+                select
+                    firstname,
+                    lastname,
+                    email,
+                    case when status = true then 'Active' else 'No active' end as status
+                from
+                    users
+            ";
+            $res = pg_query($conn, $sql);
+            if(!$res){
+                echo "Query error";
+                exit;
+            }
 
-    </table>
-    
+            while($row = pg_fetch_assoc($res)){
+                echo "<tr>";
+                echo "<td>". $row['firstname'] ."</td>";
+                echo "<td>". $row['lastname'] ."</td>";
+                echo "<td>". $row['email'] ."</td>";
+                echo "<td>". $row['status'] ."</td>";
+                echo "<td>";
+                echo "<a href=''><img src = 'icons/edit.png' width='15'></a>";
+                echo "<a href=''><img src = 'icons/search.png' width='15'></a>";
+                echo "<a href=''><img src = 'icons/trash.png' width='15'></a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>    
 </body>
 </html>
